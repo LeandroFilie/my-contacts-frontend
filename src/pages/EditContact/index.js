53d9f256a5f1ec887cbae8356e56b72a69537f0c
LeandroFilie <leandro.gfilie@gmail.com>
2023-01-04 20:01:13 -0300

    chore: added hook for safe async action

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -7,6 +7,7 @@ import ContactForm from '../../components/ContactForm';
 
 import ContactService from '../../services/ContactService';
 import toast from '../../utils/toast';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
 
 export default function EditContact() {
   const [isLoading, setIsLoading] = useState(true);
@@ -14,25 +15,30 @@ export default function EditContact() {
   const contactFormRef = useRef(null);
   const { id } = useParams();
   const history = useHistory();
+  const safeAsyncAction = useSafeAsyncAction();
 
   useEffect(() => {
     async function loadContact() {
       try {
         const contact = await ContactService.getContactById(id);
-        contactFormRef.current.setFieldsValues(contact);
-        setContactName(contact.name);
-        setIsLoading(false);
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+          setContactName(contact.name);
+          setIsLoading(false);
+        });
       } catch {
-        history.push('/');
-        toast({
-          type: 'danger',
-          text: 'Contato não encontrado',
+        safeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado',
+          });
         });
       }
     }
 
     loadContact();
-  }, [id, history]);
+  }, [id, history, safeAsyncAction]);
 
   async function handleSubmit(formData) {
     try {
