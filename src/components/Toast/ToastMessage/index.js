2364f4768eaa5a35ce99ae8679d7ba3c3663afc9
LeandroFilie <leandro.gfilie@gmail.com>
2023-01-04 16:15:13 -0300

    feat: remove toast

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -1,14 +1,34 @@
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 
-export default function ToastMessage({ text, type }) {
+export default function ToastMessage({ message, onRemoveMessage }) {
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      onRemoveMessage(message.id);
+    }, message.duration || 7000);
+
+    return () => {
+      clearTimeout(timeOutId);
+    };
+  }, [message, onRemoveMessage]);
+
+  function handleRemoveToast() {
+    onRemoveMessage(message.id);
+  }
+
   return (
-    <Container type={type}>
-      {type === 'danger' && <img src={xCircleIcon} alt="X" />}
-      {type === 'success' && <img src={checkCircleIcon} alt="Success" />}
-      <strong>{text}</strong>
+    <Container
+      type={message.type}
+      onClick={handleRemoveToast}
+      tabIndex={0}
+      role="button"
+    >
+      {message.type === 'danger' && <img src={xCircleIcon} alt="X" />}
+      {message.type === 'success' && <img src={checkCircleIcon} alt="Success" />}
+      <strong>{message.text}</strong>
     </Container>
   );
 }
--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -17,6 +17,7 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
 
   ${({ type }) => ContainerVariants[type] || ContainerVariants.default}
 
